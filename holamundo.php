77c999f18c84f15738fcffbe968eacb50fa8eb03
Adrian Zavala <133728215+Adrianz003@users.noreply.github.com>
2025-03-25 11:25:21 -0600

    Update and rename holamundo.php to holamundo.py

--- holamundo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$cadena = "Hola Mundo!";
-$lista = [];
-
-foreach (str_split($cadena) as $i) {
-    $lista[] = $i;
-    echo implode("", $lista) . "\n";
-}
---
+++ holamundo.py
@@ -0,0 +1,5 @@
+cadena = "Hola Mundo!"
+lista = []
+for i in cadena:
+    lista.append(i)
+    print(''.join(lista))
